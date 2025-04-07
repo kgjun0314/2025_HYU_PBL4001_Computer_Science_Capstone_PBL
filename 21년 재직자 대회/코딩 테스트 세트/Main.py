897e3af5a7873e545e3450f453eb163aefd538b4
kgjun0314 <unicorn9717@gmail.com>
2025-04-07 17:09:32 +0900

    update

--- 21년 재직자 대회/코딩 테스트 세트/Main.py
+++ 21년 재직자 대회/코딩 테스트 세트/Main.py
@@ -1,1 +1,44 @@
 import sys
+read = sys.stdin.readline
+
+def test(c, d, x, N):
+    tempc = c[:]
+    tempd = d[:]
+    for i in range(N):
+        if tempc[i] >= x:
+            continue
+        need = x - tempc[i]
+
+        if i > 0:
+            take = min(tempd[i - 1], need)
+            tempc[i] += take
+            tempd[i - 1] -= take
+            need -= take
+
+        if i < N - 1 and need > 0:
+            take = min(tempd[i], need)
+            tempc[i] += take
+            tempd[i] -= take
+            need -= take
+
+        if need > 0:
+            return False
+    
+    return True
+
+N, T = map(int, read().split())
+for _ in range(T):
+    scenario = list(map(int, read().split()))
+    c = scenario[::2]
+    d = scenario[1::2]
+    answer = 0
+    low = 0
+    high = 2*10**12
+    while low <= high:
+        mid = (low + high) // 2
+        if test(c, d, mid, N):
+            answer = mid
+            low = mid + 1
+        else:
+            high = mid - 1
+    print(answer)
